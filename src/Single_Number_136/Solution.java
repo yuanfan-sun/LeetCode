a661d008c85e87fae555bbd1503e34727a549a51
yuanfan-sun <82467861+yuanfan-sun@users.noreply.github.com>
2024-02-01 16:51:46 +0100

    test

---
+++ src/Single_Number_136/Solution.java
@@ -0,0 +1,11 @@
+package Single_Number_136;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
